4248f5833409f5ea94c0ef74c63be546ab954c97
Minh Nguyen <minhnguyen150403@gmail.com>
2025-06-10 17:46:08 -0500

    First creation of Spring-boot backend

---
+++ src/main/java/com/minhnguyen/AI_habit_track/AiHabitTrackApplication.java
@@ -0,0 +1,14 @@
+package com.minhnguyen.AI_habit_track;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiHabitTrackApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AiHabitTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minhnguyen/AI_habit_track/AiHabitTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhnguyen.AI_habit_track;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiHabitTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
